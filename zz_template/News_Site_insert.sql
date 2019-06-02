14d903d542c3913a7f81812ae0d1c40bd2839ea3
binhnguyen052 <44772206+binhnguyen052@users.noreply.github.com>
2019-06-02 18:38:12 +0700

    Update News_Site_insert.sql

--- zz_template/News_Site_insert.sql
+++ zz_template/News_Site_insert.sql
@@ -1,3 +1,6 @@
+USE `News_Site` ;
+
+
 INSERT INTO `news_site`.`user_permission` (`id`, `key`, `id_delete`) VALUES ('1', 'admin', '0');
 INSERT INTO `news_site`.`user_permission` (`id`, `key`, `id_delete`) VALUES ('2', 'editor', '0');
 INSERT INTO `news_site`.`user_permission` (`id`, `key`, `id_delete`) VALUES ('3', 'writer', '0');
