e62adea58e8d65ba0729696340c55c56c1321341
binhnguyen052 <44772206+binhnguyen052@users.noreply.github.com>
2019-06-02 18:30:40 +0700

    upload template (link: drive)

---
+++ zz_template/News_Site_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `news_site`.`user_permission` (`id`, `key`, `id_delete`) VALUES ('1', 'admin', '0');
+INSERT INTO `news_site`.`user_permission` (`id`, `key`, `id_delete`) VALUES ('2', 'editor', '0');
+INSERT INTO `news_site`.`user_permission` (`id`, `key`, `id_delete`) VALUES ('3', 'writer', '0');
+INSERT INTO `news_site`.`user_permission` (`id`, `key`, `id_delete`) VALUES ('4', 'subcriber', '0');
