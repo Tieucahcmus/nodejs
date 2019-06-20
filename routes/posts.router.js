625b3e07870b0328ea48fbcbc1e41b4bd3f2c9a8
binhnguyen052 <44772206+binhnguyen052@users.noreply.github.com>
2019-06-20 11:47:59 +0700

    thêm route,  hiển thị xem danh sách category subcategory  router.posts.js

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -56,20 +56,29 @@ router.get("/category/:id", (req, res, next) => {
 });
 
 //chỗ này sẽ hiển thị chi tiết 1 bài báo
-//slug_name: tên không dấu
-router.get("/single/:slug_name", (req, res, next) => {
-  console.log("posts/single/slug_name");
-  var slug_name = req.params.slug_name;
-  console.log(slug_name);
+//slug_title: tên không dấu
+router.get("/single/:slug_title", (req, res, next) => {
+  console.log("posts/single/slug_title");
+  var slug_title = req.params.slug_title;
+  console.log(slug_title);
 
-  if (!slug_name || slug_name.length === 0) {
+  if (!slug_title || slug_title.length === 0) {
     res.render("404", {
       layout: false
     });
     return;
   }
 
-  postModel.singleByExist("slug_title", slug_name, 0).then(rows => {
+  db.load(
+    `select post.*, 
+    category.name as 'catname', category.slug_name as 'cat_slugname',
+    subcategory.name as 'subname', subcategory.slug_name as 'sub_slugname'
+      from post join category on post.id_category = category.id 
+      join subcategory on post.id_subcategory = subcategory.id
+      where post.slug_title = '${slug_title}' 
+      and post.is_delete = 0 and category.is_delete = 0 and subcategory.is_delete = 0`
+  ).then(rows => {
+    console.log(rows.length);
     if (rows.length > 0) {
       res.render("view_posts/single-post_publish", {
         error: false,
@@ -85,7 +94,7 @@ router.get("/single/:slug_name", (req, res, next) => {
   });
 });
 
-//chỗ này sẽ hiển thị các bài báo sau khi nhấn vào category hoặc subcategory
+//chỗ này sẽ hiển thị các bài báo sau khi nhấn vào category
 //slug-title: tên không dấu
 router.get("/menu/:slug_title", (req, res, next) => {
   console.log("posts/meunu/slug-title");
@@ -100,19 +109,28 @@ router.get("/menu/:slug_title", (req, res, next) => {
   }
 
   db.load(
-    `select category.name as 'catname', category.slug_name , post.* 
+    `select post.* ,
+            category.name as 'catname', category.slug_name as 'cat_slugname'
       from post join category on post.id_category = category.id
       where category.slug_name = '${slug_title}' and category.is_delete = 0 and post.is_delete = 0`
   ).then(rows => {
     if (rows.length > 0) {
       console.log(rows.length);
-      // console.log(rows);
-      res.render("view_posts/group-post", {
-        error: false,
-        post_publish: rows
-        // post_categories: res.locals.post_categories
-      });
-    } else {
+      db.load(
+        `select post_tag.*, tag.name as 'tagname'
+          from post_tag join tag on post_tag.id_tag = tag.id`
+      )
+        .then(post_tags => {
+          console.log(post_tags.length);
+          res.render("view_posts/group-post", {
+            error: false,
+            post_publish: rows,
+            post_tags: post_tags
+          });
+        })
+        .catch(next);
+    } //
+    else {
       res.render("404", {
         // error: true
         layout: false
@@ -123,11 +141,68 @@ router.get("/menu/:slug_title", (req, res, next) => {
   //res.render("home");
 });
 
+//chỗ này sẽ hiển thị các bài báo sau khi nhấn vào category/subcategory
+//slug-title: tên không dấu
+router.get("/menu/:slug_cat/:slug_sub", (req, res, next) => {
+  console.log("posts/meunu/slug-cat/slug-sub");
+  var slug_cat = req.params.slug_cat;
+  var slug_sub = req.params.slug_sub;
+  console.log(req.params);
+  console.log(slug_cat);
+  console.log(slug_sub);
+
+  if (
+    !slug_cat ||
+    slug_cat.length === 0 ||
+    (!slug_sub || slug_sub.length === 0)
+  ) {
+    res.render("404", {
+      layout: false
+    });
+    return;
+  }
+
+  db.load(
+    `select post.*,  
+		        category.name as 'catname', category.slug_name as 'cat_slugname',
+            subcategory.name as 'subname', subcategory.slug_name as 'sub_slugname' 
+      from post join category on post.id_category = category.id
+      join subcategory on  post.id_subcategory = subcategory.id  
+      where category.slug_name = '${slug_cat}' and subcategory.slug_name = '${slug_sub}'
+      and category.is_delete = 0 and post.is_delete = 0 and subcategory.is_delete = 0`
+  ).then(rows => {
+    if (rows.length > 0) {
+      console.log(rows.length);
+      db.load(
+        `select post_tag.*, tag.name as 'tagname'
+          from post_tag join tag on post_tag.id_tag = tag.id`
+      )
+        .then(post_tags => {
+          console.log(post_tags.length);
+          res.render("view_posts/group-post", {
+            error: false,
+            is_have_subcategory: true,
+            post_publish: rows,
+            post_tags: post_tags,
+          });
+        })
+        .catch(next);
+    } //
+    else {
+      res.render("404", {
+        // error: true
+        layout: false
+      });
+    }
+  });
+
+  //res.render("home");
+});
+
+
 //chỗ này sẽ hiển thị các bài báo sau khi nhấn vào category hoặc subcategory
 //tagname: tên tên tag
 router.get("/tag/:tagname", (req, res, next) => {
-  
-  
   res.render("home");
 });
 
